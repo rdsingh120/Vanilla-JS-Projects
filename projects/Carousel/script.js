d2ada4967012d71551c8ca2d5c48fb2e05374fd2
Ripudaman Singh <rdsingh120@gmail.com>
2024-03-07 08:23:58 -0500

    arrays changed to object

--- projects/Carousel/script.js
+++ projects/Carousel/script.js
@@ -1,35 +1,42 @@
 const data = [
-    [
-        "Wade Wilson",
-        "Deadpool",
-        `"It's a big house. It's weird I only ever see two of you. 
-        Almost like the studio couldn't afford another X-Man."`
-    ],
-    [
-        "Logan",
-        "Wolverine",
-        `"We lost Scott. We lost the professor. If we don't fight now, everything they stood for will die with them."`
-    ],
-    [
-        "Clarke Kent",
-        "Superman",
-        `“It Was Krypton That Made Me Superman, But It's The Earth That Makes Me Human.”`
-    ],
-    [
-        "Jack Napier",
-        "Joker",
-        `“Smile, because it confuses people. Smile, because it's easier than explaining what is killing you inside.”`
-    ],
-    [
-        "Bruce Wayne",
-        "Batman",
-        `“I Wear A Mask. And That Mask, It’s Not To Hide Who I Am, But To Create What I Am.”`
-    ]
+    {
+        image: "images/photo0.jpg",
+        name: "Wade Wilson",
+        alias: "Deadpool",
+        quote: `"It's a big house. It's weird I only ever see two of you. Almost like the studio couldn't afford another X-Man."`
+    },
+    {
+        image: "images/photo1.jpg",
+        name: "Logan",
+        alias: "Wolverine",
+        quote: `"We lost Scott. We lost the professor. If we don't fight now, everything they stood for will die with them."`
+
+    },
+    {
+        image: "images/photo2.jpg",
+        name: "Clarke Kent",
+        alias: "Superman",
+        quote: `“It Was Krypton That Made Me Superman, But It's The Earth That Makes Me Human.”`
+    },
+    {
+        image: "images/photo4.jpg",
+        name: "Bruce Wayne",
+        alias: "Batman",
+        quote: `“I Wear A Mask. And That Mask, It’s Not To Hide Who I Am, But To Create What I Am.”`
+
+
+    },
+    {
+        image: "images/photo3.jpg",
+        name: "Jack Napier",
+        alias: "Joker",
+        quote: `“Smile, because it confuses people. Smile, because it's easier than explaining what is killing you inside.”`
+    },
 ];
 
 const cardName = document.getElementById("name");
 const position = document.getElementById("position");
-const review = document.getElementById("review");
+const quote = document.getElementById("quote");
 const backward = document.getElementById("backward");
 const forward = document.getElementById("forward");
 const surprise = document.getElementById("surprise");
@@ -67,10 +74,10 @@ surprise.addEventListener("click", function() {
 });
 
 function info() {
-    image.src = `images/photo${number}.jpg`
-    cardName.textContent = data[number][0];
-    position.textContent = data[number][1];
-    review.textContent = data[number][2];  
+    image.src = data[number].image;
+    cardName.textContent = data[number].name;
+    position.textContent = data[number].alias;
+    quote.textContent = data[number].quote;  
 }
 
 info();
